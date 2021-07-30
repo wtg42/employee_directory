89434e056d4573a9e1541dddeade01cdb85f49b0
weiting <weiting.shi1982@gmail.com>
2021-07-30 17:11:57 +0800

    Livewire component 寫入 DB 測試。

--- tests/Feature/DirectoryTest.php
+++ tests/Feature/DirectoryTest.php
@@ -2,15 +2,20 @@
 
 namespace Tests\Feature;
 
+use App\Http\Livewire\NewDirectoryForm;
+use App\Models\Directory;
 use App\Models\User;
 use Laravel\Sanctum\Sanctum;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 class DirectoryTest extends TestCase
 {
+    // 測試完清空 Table
+    // use RefreshDatabase;
+
     /**
      * Test route directory.index
-     *
      * @return void
      */
     public function testRouteDirectory()
@@ -19,6 +24,8 @@ class DirectoryTest extends TestCase
 
         $response->assertStatus(200);
 
+        $response->assertSeeLivewire('directory-list');
+
         $response->assertSeeTextInOrder(['Directories List', '中文姓名']);
     }
 
@@ -26,9 +33,9 @@ class DirectoryTest extends TestCase
      *  Test route directory.create with authenticated user
      *  @return void
      */
-    public function testRouteDirectoryCreateLogin()
+    public function testRouteDirectoryCreateLoginAndCreateNew()
     {
-        $response = Sanctum::actingAs(User::factory()->make());
+        $response = Sanctum::actingAs(User::factory()->create());
 
         $response = $this->get(route('directory.create'));
 
@@ -36,7 +43,22 @@ class DirectoryTest extends TestCase
 
         $response->assertViewIs('dashboard-directory-create');
 
+        $response->assertSeeLivewire('new-directory-form');
+
         $response->assertSeeTextInOrder(['New Directory Form', '新增通訊錄', '中文姓名', '新增']);
+
+        $_testEmployeeNum = rand(100000, 999999);
+        Livewire::test(NewDirectoryForm::class)
+            ->set('employee_number', $_testEmployeeNum)
+            ->set('chinese_name', '喬' . rand(1, 999999))
+            ->set('english_name', 'joe' . rand(1, 999999))
+            ->set('email', rand(1, 999999) . '@gmail.com')
+            ->set('phone', '0' . rand(900000000, 999999999))
+            ->set('ext', rand(100, 999))
+            ->set('dept', rand(1, 10))
+            ->call('submitForm');
+
+        $this->assertTrue(Directory::whereEmployee_number($_testEmployeeNum)->exists());
     }
 
     /**
