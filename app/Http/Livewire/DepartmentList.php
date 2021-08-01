7d859b33ff4022c1505a1901fe1895af807139ac
weiting <weiting.shi1982@gmail.com>
2021-08-02 00:16:49 +0800

    Add Livewire\WithPagination 分頁

--- app/Http/Livewire/DepartmentList.php
+++ app/Http/Livewire/DepartmentList.php
@@ -4,9 +4,12 @@ namespace App\Http\Livewire;
 
 use App\Models\Department;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class DepartmentList extends Component
 {
+    use WithPagination; // livewire分頁用 沒加會變成reload分頁
+
     public function render()
     {
         $dept = Department::paginate(5);
